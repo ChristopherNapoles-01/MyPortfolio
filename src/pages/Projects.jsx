0420ebce01d52ea4da685f839b3fe88759055d1d
ChristopherNapoles-01 <christophernapoles35@gmail.com>
2022-07-19 10:21:20 +0800

    Changed animation duration on project page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -16,7 +16,7 @@ const Projects = () => {
         setOpenView(false);
     }
     useEffect(()=>{
-        Aos.init({duration:3000});
+        Aos.init({duration:2000});
     },[])
     return(
         <div className="body list-body">
